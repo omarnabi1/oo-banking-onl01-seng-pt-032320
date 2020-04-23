70ff7b41ed1ddb0314f2604ad14647b315fe5dda
Omar Nabi <onabi20@gmail.com>
2020-04-23 04:26:36 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
 
+  attr_accessor :balance, :status 
+  attr_reader :name 
+  
+  def initialize(name)
+    @balance = 1000
+    @name = name 
+    @status = 'open'
+  end
+  
+
+
+
+
 end
