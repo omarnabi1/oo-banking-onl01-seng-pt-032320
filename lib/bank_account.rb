e03b38e9f7912c33ef93c75a721c3c1a83712faa
Omar Nabi <onabi20@gmail.com>
2020-04-23 04:29:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,8 +9,24 @@ class BankAccount
     @status = 'open'
   end
   
-
-
-
-
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}"
+  end
+  
+  def valid?
+    if (@status == 'open' && @balance > 0)
+      true 
+    else 
+      false 
+    end
+  end
+  
+  def close_account
+    @status = 'closed'
+  end
+  
 end
