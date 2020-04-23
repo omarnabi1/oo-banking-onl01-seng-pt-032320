2d8e5a481181110263cb20dbff3dccaaf289a9ca
Omar Nabi <onabi20@gmail.com>
2020-04-23 04:31:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
-  # your code here
+  attr_accesor :receiver, :sender, :amount, :status
+  
+  def initialize(sender, receiver, amount=0)
+    @receiver = receiver
+    @sender = sender
+    @amount = amount
+    @status = 'pending'
+  end
+  
+    
+    
+    
+    
+    
 end
